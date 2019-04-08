858b2e62e2875d26db912a525104f164462b456e
Diana Badas <diana.badas@gmail.com>
2019-04-08 23:26:10 +0000

    Automatically backed up by Learn

--- spec/sinatra_logging_in_and_out_spec.rb
+++ spec/sinatra_logging_in_and_out_spec.rb
@@ -85,7 +85,7 @@ describe 'ApplicationController' do
       expect(last_response.body).to include("<h1>Welcome skittles123</h1>")
       expect(last_response.body).to include("<h3>Your Balance: 1000.0</h3>")
     end
-  =end
+=end
   end
 
   describe "GET '/logout'" do
